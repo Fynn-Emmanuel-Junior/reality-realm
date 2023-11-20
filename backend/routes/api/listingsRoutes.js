fdbe724a0f909a9dbfeb5425a1f7dda60f6daa44
Fynn Emmanuel Junior <fynn.emmanuel.junior@gmail.com>
2023-11-20 16:33:48 +0000

    Created Search api route

--- backend/routes/api/listingsRoutes.js
+++ backend/routes/api/listingsRoutes.js
@@ -5,7 +5,8 @@ import {
     getAllListings,
     deletelisting,
     updatelisting,
-    getListing
+    getListing,
+    getListings
 } from '../../controllers/listingsController.js'
 
 const router = express.Router()
@@ -15,6 +16,7 @@ router.post('/create',authMiddleware,createListings)
 router.delete('/delete/:id',authMiddleware,deletelisting)
 router.put('/update/:id',authMiddleware,updatelisting)
 router.get('/getlisting/:id',getListing)
+router.get('/get',getListings)
 
 
 export default router
