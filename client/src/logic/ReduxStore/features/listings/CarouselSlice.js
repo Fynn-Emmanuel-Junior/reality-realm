b20ec0fee5e61a4ac3beffe92cdaf1ec99658bc7
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-05-05 16:07:44 +0000

    Update logic

--- client/src/logic/ReduxStore/features/listings/CarouselSlice.js
+++ client/src/logic/ReduxStore/features/listings/CarouselSlice.js
@@ -1,1 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    images: []
+}
+
+const ImageSlice = createSlice({
+    name: 'images',
+    initialState,
+    reducers: {
+        setImages: (state,action) => {
+            state.images = action.payload;
+        }
+    }
+})
+
+export default ImageSlice.reducer
