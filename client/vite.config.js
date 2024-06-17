de82f717529ee2dec2bb336479aa321d6a00d9ae
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-06-17 11:53:36 +0000

    Update client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -12,4 +11,7 @@ export default defineConfig({
     }
   },
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  }
+});
