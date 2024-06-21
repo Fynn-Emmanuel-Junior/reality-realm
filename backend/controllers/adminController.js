a2b92b14780c661b4f037f2242e63c2731cdf7d5
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-06-21 18:09:36 +0000

    Update backend

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,34 +1,62 @@
-import adminModel from '../models/AdminModel.js';
+import AdminModel from '../models/AdminModel.js';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken'
-import validator from 'validator'
+import jwt from 'jsonwebtoken';
+import validator from 'validator';
 
-export const createAdminController = async(req,res) => {
-    const { adminName, email, password,adminkey } = req.body
-    if(!validator.isEmail(email)) return res.status(404).json({message: 'Email is not valid'});
-    if(!validator.isStrongPassword(password)) return res.status(404).json({message: 'Password is not strong enough'});
-    const CheckIfEmailExists = await adminModel.findOne({email})
-    if(CheckIfEmailExists) return res.status(409).json({message: "Admin already exists"})
-        
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
+export const createAdminController = async (req, res) => {
+    const { adminName, email, password, adminkey } = req.body;
+
+    if(adminkey !== 'RR10_REG_') return res.status(403).json({message: 'Cannot create admin'});
+
+    if (!validator.isEmail(email)) return res.status(400).json({ message: 'Email is not valid' });
+    if (!validator.isStrongPassword(password)) return res.status(400).json({ message: 'Password is not strong enough' });
+
+    const CheckIfEmailExists = await AdminModel.findOne({ email });
+    if (CheckIfEmailExists) return res.status(409).json({ message: "Admin already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     try {
-        const admin = await adminModel.create({
+        const admin = await AdminModel.create({
             adminName,
             email,
-            password:  hashedPassword,
+            password: hashedPassword,
             AdminKey: adminkey
-        })
-        
-        const {_id, ...rest} = admin
+        });
+
+        const accessToken = jwt.sign(
+            { 'adminId': admin._id },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '10m' }
+        );
+
+        const refreshToken = jwt.sign(
+            { 'adminId': admin._id },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        res.cookie('jwt', accessToken, {
+            httpOnly: true,
+            sameSite: 'None',
+            secure: process.env.NODE_ENV !== 'development'
+        });
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            sameSite: 'None',
+            secure: process.env.NODE_ENV !== 'development'
+        });
+
+        const { _id,...rest } = admin;
+
         res.status(201).json({
             statusCode: 201,
             message: 'Admin created',
-            adminId: _id
+            admin: { _id}
         });
-    }catch(err) {
-        res.status(400).json({message: `cannot create admin: ${err.message}`})
+    } catch (err) {
+        res.status(500).json({ message: `Cannot create admin: ${err.message}` });
     }
-
-}
+};
