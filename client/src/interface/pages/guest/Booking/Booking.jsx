e3ef4e8656adadb8e21c740bff6e97e4e83a32cf
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-06-29 09:47:30 +0000

    Update client

--- client/src/interface/pages/guest/Booking/Booking.jsx
+++ client/src/interface/pages/guest/Booking/Booking.jsx
@@ -3,12 +3,23 @@ import { IoIosArrowBack } from "react-icons/io";
 
 const Booking = () => {
   return (
-    <div className='w-11/12 mx-auto md:hidden'>
-        <header className='flex items-center gap-10 mt-5'>
-            <IoIosArrowBack size={20}/>
-            <p>Confirm and Book Appointment</p>
-        </header>
+    <div className='md:hidden'>
+        <div>
+            <div className='w-11/12 mx-auto'>
+                <header className='flex items-center gap-20 mt-5 pb-5'>
+                    <IoIosArrowBack size={20}/>
+                    <p className='font-semibold'>Confirm and book appointment</p>
+                </header>
+            </div>
+        </div>
+        <hr />
+        <div>
+            <div className='w-11/12 mx-auto'>
+
+            </div>
+        </div>
     </div>
+    
   )
 }
 
