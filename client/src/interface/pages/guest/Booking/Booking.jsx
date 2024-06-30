5ab4134f09e9f59c49e2f2a7373e81834851c771
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-06-30 19:32:38 +0000

    Update client

--- client/src/interface/pages/guest/Booking/Booking.jsx
+++ client/src/interface/pages/guest/Booking/Booking.jsx
@@ -25,16 +25,16 @@ const Booking = () => {
                     <p className='font-semibold'>Confirm and book appointment</p>
                 </header>
             </div>
-        </div>
+        </div> 
         <hr />
         <div>
             <div className='w-11/12 mx-auto flex items-center gap-3 my-7'>
                 <div className='w-28 h-24'>
-                    <img src={listing.imageurls[0]} alt='profile_img' className='w-full h-full rounded-2xl'/>
+                    <img src={listing?.imageurls[0]} alt='profile_img' className='w-full h-full rounded-2xl'/>
                 </div>
                 <div>
-                    <h3 className='font-semibold'>{listing.name} - {listing.description} </h3>
-                    <div>{listing.address}</div>
+                    <h3 className='font-semibold'>{listing?.name} - {listing?.description} </h3>
+                    <div>{listing?.address}</div>
                     <div className='flex gap-2 items-center'>
                         <IoIosStar size={20}/>
                         <p className='font-medium'>5.00(4)</p> 
@@ -43,6 +43,12 @@ const Booking = () => {
             </div>
         </div>
         <div className='bg-gray-200 w-full h-3'/>
+        <div className='w-11/12 mx-auto my-7'>
+            <h3 className='font-semibold text-2xl'> Your Appointment </h3>
+            <div>
+
+            </div>
+        </div>
     </div>
     
   )
