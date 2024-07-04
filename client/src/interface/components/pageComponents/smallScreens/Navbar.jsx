b2651270e505f13dbbe524cf30475c2f828c9866
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-07-04 20:26:24 +0000

    Update client

--- client/src/interface/components/pageComponents/smallScreens/Navbar.jsx
+++ client/src/interface/components/pageComponents/smallScreens/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { HiOutlineMenu } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import Menu from '../../pageComponents/Menu';
