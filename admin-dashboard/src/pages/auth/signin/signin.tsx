6a87d384fa192895c870c2b7e748393c62a29b61
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-07-09 13:19:40 +0000

    Update admin-dashboard

--- admin-dashboard/src/pages/auth/signin/signin.tsx
+++ admin-dashboard/src/pages/auth/signin/signin.tsx
@@ -1,5 +1,5 @@
 import { Fragment} from "react";
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
 import { Link} from "react-router-dom";
 import { imagesData } from '../../../common/commonimages';
 import { useSignIn } from "../../../hooks/useSignIn";
