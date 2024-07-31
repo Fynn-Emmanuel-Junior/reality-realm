42ed9066b358e2e71d93130a50aaa6ec2c36aa0c
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-07-31 11:18:32 +0000

    Update client

--- client/src/interface/components/customs/ListingSkeleton.jsx
+++ client/src/interface/components/customs/ListingSkeleton.jsx
@@ -2,12 +2,12 @@
 const ListingSkeleton = () => {
   return (
     <div className='w-11/12 mx-auto mt-5'>
-        <div className='w-11/12 md:w-[40%] flex flex-col items-center'>
+        <div className='w-11/12 md:w-4/5 lg:w-[40%] flex flex-col items-center'>
             <div className="w-full h-8 animate-pulse bg-stone-200 rounded-md my-5"/>	
         </div>
 
         <div className='lg:flex lg:items-center lg:justify-center lg:gap-3 animate-pulse'>
-            <div className='w-full md:w-7/12 h-44 bg-stone-200 animate-pulse  rounded-lg md:rounded-l-lg' />
+            <div className='w-full md:w-4/5 lg:w-7/12 h-44 bg-stone-200 animate-pulse  rounded-lg md:rounded-l-lg' />
             <div className='hidden lg:w-5/12 lg:h-44 lg:grid lg:grid-cols-2 lg:gap-2 animate-pulse'>
                 <div className='bg-stone-200 rounded-l-lg h-20'/>
                 <div className='bg-stone-200 rounded-l-lg h-20'/>
